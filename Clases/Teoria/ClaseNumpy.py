95f9962aec771dc1b756c822dd4c8dee407b54c8
Manuel Alejandro Bastidas Ospina <manuba2309@gmail.com>
2024-10-24 11:04:25 -0500

    Primer commit

---
+++ Clases/Teoria/ClaseNumpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([1,2])
+print(a)
+
+matriz_identidad = np.eye()
+print(matriz_identidad)
---
+++ Clases/Teoria/clase23-10-2024.py
@@ -0,0 +1,12 @@
+#Variables cualitativas (Nominales, Ordinales y Binarias) y cuantitativas (Discretas y continuas)
+#Datos estructurados: Tablas, JSON ---------- Datos no estructurados: Videos, Sonidos e Imagenes
+#Preprocesamiento de datos: conjunto de tecnicas que aseguran la viabilidad y la eficiencia, dejar los datos de forma adecuada para luego pasarlos a un modelo de ML.
+   #Limpieza de datos: Datos nulos (NaN), datos duplicados, datos mal escritos, datos atipicos, tipos de datos inconsistentes.
+   #Transformacion de datos: Filtrar los datos, traduccion de datos cualitativos a numeros o estructuras matematicas, dejar los datos en una misma escala (normalizacion)
+   #Reduccion de datos: Reduccion de dimensionalidad, quitar columnas innecesarias - PCA Algorithm
+
+#Procesamiento de datos
+   #Seleccionar la data y dividirlos en entrenamiento, datos para que el modelo aprenda y los datos de test es para evaluar si el modelo predice correctamente.
+   #Procesar la data, seleccionar las columnas relevantes
+
+print("hola")
