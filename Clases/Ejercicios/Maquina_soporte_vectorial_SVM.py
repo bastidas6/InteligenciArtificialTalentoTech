52c448b4f5df3af32b2a22da7cb5ac572f1929dd
Manuel Alejandro Bastidas Ospina <manuba2309@gmail.com>
2024-11-06 19:04:53 -0500

    Teoria de algoritmo SVM

---
+++ Clases/Ejercicios/Maquina_soporte_vectorial_SVM.py
@@ -0,0 +1,5 @@
+#SVM = Es un algortimo de aprendizaje supervisado, para tareas de clasificación y regresión.
+# El objetivo es encontrar la separacion entre los puntos, o un hiperplano optimo que maximice la separación de los puntos de datos
+
+import pandas as pd
+import numpy as np
