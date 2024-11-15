96ee14f1624481c975df08e829d334b772004f23
Manuel Alejandro Bastidas Ospina <manuba2309@gmail.com>
2024-11-15 07:49:10 -0500

    Se ctaegoriza la variable dependiente gravedad del homicidio

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -17,6 +17,7 @@ print("El número de filas y de columnas del dataset es:\n", df.shape)
 #TIPOS DE DATOS
 # Identificar los tipos de datos de cada columna
 print("Tipos de datos de cada columna:\n", df.dtypes)
+print(df.head(5))
 
 #Categorizamos la columna sexo
 d1 = {'Hombre': 1, 'Mujer': 2, 'Indeterminado':3}
@@ -35,11 +36,16 @@ df['Grupo de edad de la victima']=df['Grupo de edad de la victima'].map(d3)
 #Categorizamos la columna Grupo de mes del hecho
 d4 = {'Enero': 1, 'enero': 1,'Febrero': 2,'febrero': 2, 'Marzo': 3, 'marzo': 3,'Abril': 4,'abril': 4, 'Mayo': 5,  'mayo': 5, 'Junio': 6, 'junio': 6, 'Julio': 7, 'julio': 7,'Agosto': 8,'agosto': 8 ,'Septiembre': 9, 'septiembre': 9,'Octubre': 10,'octubre': 10 ,'Noviembre': 11,'noviembre': 11 ,'Diciembre': 12, 'diciembre': 12, 'Sin Información':13}
 df['Mes del hecho']=df['Mes del hecho'].map(d4)
-print(df.head(5))
+
 
 #Categorizamos la columna Departamento del hecho DANE
 d5 = {'Antioquia': 1, 'Amazonas': 2, 'Arauca': 3, 'Archipiélago de San Andrés, Providencia y Santa Catalina': 4, 'Atlántico': 5, 'Bogotá, D.C.': 6, 'Bolívar': 7, 'Boyacá': 8, 'Córdoba': 9, 'Caldas': 10, 'Caquetá': 11, 'Casanare': 12, 'Cauca': 13, 'Cesar': 14, 'Chocó': 15, 'Cundinamarca': 16, 'Guainía': 17, 'Guaviare': 18, 'Huila': 19, 'La Guajira': 20, 'Magdalena': 21, 'Meta': 22, 'Nariño': 23, 'Norte de Santander': 24, 'Putumayo': 25, 'Quindío': 26, 'Risaralda': 27, 'Santander': 28, 'Sucre': 29, 'Tolima':30, 'Valle del Cauca': 31, 'Vaupés': 32, 'Vichada': 33, 'Sin Información': 34, 'Sin información': 34}
 df['Departamento del hecho DANE']=df['Departamento del hecho DANE'].map(d5)
+
+
+#Categorizamos la columna dependiente Magnitud del homicidio
+d6 = {'Alto Riesgo': 1, 'Bajo Riesgo': 2}
+df['Magnitud del homicidio']=df['Magnitud del homicidio'].map(d6)
 print(df.head(5))
 
 
@@ -55,6 +61,7 @@ print("Cantidad de datos faltantes en cada columna:\n", faltantes_por_columna)
 
 #CALCULO DE LA CORRELACION
 print("La correlación es: ", df[["Año del hecho","Sexo de la victima"]].corr())
+print(df.head(5))
 
 
 
