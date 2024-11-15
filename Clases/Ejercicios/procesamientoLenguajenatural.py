7d58e982252d81aa3e37d404624ec13692f46dd4
Manuel Alejandro Bastidas Ospina <manuba2309@gmail.com>
2024-11-14 21:34:34 -0500

    Se inicia proyecto y se categorizan algunos variables

--- Clases/Ejercicios/procesamientoLenguajenatural.py
+++ Clases/Ejercicios/procesamientoLenguajenatural.py
@@ -1,3 +1,66 @@
 #Interpretar lenguaje natural: Un sistema que etsudia una entrada del ser humano y devuelve una salida en el mismo idioma.
 #Se debe generar la comunicacion con la persona, ya sea por texto o por medio de la voz.
 #El NLP es una parte de la IA
+
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer 
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+import pandas as pd
+import re
+
+df = pd.read_csv("Datasets/reviews.csv")
+print(df.sample(10))
+
+corpus = pd.Series(df.Review.tolist()).astype(str)
+print(corpus[727])
+
+def limpiar_texto(corpus, ls_permitir):   
+    corpus_limpio = []
+    for fila in corpus:
+        qs = []
+        for palabra in fila.split():
+            if palabra not in ls_permitir:
+                p1 = re.sub(pattern='[^a-zA-Z0-9]',repl=' ',string=palabra)
+                p1 = p1.lower()
+                qs.append(p1)
+            else: 
+                qs.append(palabra)        
+        corpus_limpio.append(' '.join(qs))
+    return pd.Series(corpus_limpio)
+
+def remover_palabras_vacias(corpus):
+    palabras = ['who', 'what', 'when', 'why', 'how', 'which', 'where', 'whom']
+    stop = set(stopwords.words('english'))
+    for palabra in palabras:
+        stop.remove(palabra)
+    corpus = [[x for x in x.split() if x not in stop] for x in corpus]
+    return corpus
+
+def lematizar(corpus):
+    lem = WordNetLemmatizer()
+    corpus = [[lem.lemmatize(x, pos = 'v') for x in x] for x in corpus]
+    return corpus
+
+def stem(corpus, stem_type = None):
+    if stem_type == 'snowball':
+        stemmer = SnowballStemmer(language = 'english')
+        corpus = [[stemmer.stem(x) for x in x] for x in corpus]
+    else :
+        stemmer = PorterStemmer()
+        corpus = [[stemmer.stem(x) for x in x] for x in corpus]
+    return corpus
+
+palabras_comunes = ['U.S.A', 'Mr.', 'Mrs.', 'D.C.']
+corpus = limpiar_texto(corpus, palabras_comunes)
+corpus = remover_palabras_vacias(corpus)
+#corpus = [[x for x in ] for x in corpus]
+corpus = [[x for x in oraciones] for oraciones in corpus]
+corpus = lematizar(corpus)
+corpus = stem(corpus, 'snowball')    
+corpus = [' '.join(x) for x in corpus]    
+
+print(corpus[727])
--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 
 #Cargamos el dataset
 df = pd.read_csv("Proyecto/Homicidios_Colombia.csv", delimiter=";")
+faltantes_por_columna = df.isnull().sum()
+print("Cantidad de datos faltantes iniciales en cada columna:\n", faltantes_por_columna)
+
 
 #Miramos la cantidad de filas y de columnas del dataset
 print("El número de filas y de columnas del dataset es:\n", df.shape)
@@ -15,10 +18,43 @@ print("El número de filas y de columnas del dataset es:\n", df.shape)
 # Identificar los tipos de datos de cada columna
 print("Tipos de datos de cada columna:\n", df.dtypes)
 
+#Categorizamos la columna sexo
+d1 = {'Hombre': 1, 'Mujer': 2, 'Indeterminado':3}
+df['Sexo de la victima']=df['Sexo de la victima'].map(d1)
+
+
+#Categorizamos la columna dia del hecho
+d2 = {'Lunes': 1,'lunes': 1, 'Martes': 2,'martes': 2 ,'Miércoles': 3,'miércoles': 3, 'Jueves': 4, 'jueves': 4, 'Viernes': 5, 'viernes': 5, 'Sábado': 6, 'sábado': 6,'Domingo': 7, 'domingo': 7, 'Sin Información': 8}
+df['Dia del hecho']=df['Dia del hecho'].map(d2)
+
+
+#Categorizamos la columna Grupo de edad de la victima
+d3 = {'(00 a 04)': 1,'(05 a 09)': 2,'(10 a 14)': 3, '(15 a 17)': 4, '(18 a 19)': 5, '(20 a 24)': 6, '(25 a 29)': 7, '(30 a 34)': 8, '(35 a 39)': 9, '(40 a 44)': 10, '(45 a 49)': 11, '(50 a 54)': 12,'(55 a 59)': 13, '(60 a 64)': 14, '(65 a 69)': 15, '(70 a 74)': 16, '(75 a 79)': 17, '(80 y más)': 18, 'Por determinar': 19}
+df['Grupo de edad de la victima']=df['Grupo de edad de la victima'].map(d3)
+
+#Categorizamos la columna Grupo de mes del hecho
+d4 = {'Enero': 1, 'enero': 1,'Febrero': 2,'febrero': 2, 'Marzo': 3, 'marzo': 3,'Abril': 4,'abril': 4, 'Mayo': 5,  'mayo': 5, 'Junio': 6, 'junio': 6, 'Julio': 7, 'julio': 7,'Agosto': 8,'agosto': 8 ,'Septiembre': 9, 'septiembre': 9,'Octubre': 10,'octubre': 10 ,'Noviembre': 11,'noviembre': 11 ,'Diciembre': 12, 'diciembre': 12, 'Sin Información':13}
+df['Mes del hecho']=df['Mes del hecho'].map(d4)
+print(df.head(5))
+
+#Categorizamos la columna Departamento del hecho DANE
+d5 = {'Antioquia': 1, 'Amazonas': 2, 'Arauca': 3, 'Archipiélago de San Andrés, Providencia y Santa Catalina': 4, 'Atlántico': 5, 'Bogotá, D.C.': 6, 'Bolívar': 7, 'Boyacá': 8, 'Córdoba': 9, 'Caldas': 10, 'Caquetá': 11, 'Casanare': 12, 'Cauca': 13, 'Cesar': 14, 'Chocó': 15, 'Cundinamarca': 16, 'Guainía': 17, 'Guaviare': 18, 'Huila': 19, 'La Guajira': 20, 'Magdalena': 21, 'Meta': 22, 'Nariño': 23, 'Norte de Santander': 24, 'Putumayo': 25, 'Quindío': 26, 'Risaralda': 27, 'Santander': 28, 'Sucre': 29, 'Tolima':30, 'Valle del Cauca': 31, 'Vaupés': 32, 'Vichada': 33, 'Sin Información': 34, 'Sin información': 34}
+df['Departamento del hecho DANE']=df['Departamento del hecho DANE'].map(d5)
+print(df.head(5))
+
+
+#ELIMINAMOS COLUMNAS
+df.drop(columns=['Estado', 'Lesion fatal de causa externa', 'Municipio del hecho DANE', 'ID'], inplace=True)
+print(df.head(5))
+print(df.describe())
+
 #CANTIDAD DE DATOS FALTANTES
 # Detectar la cantidad de datos faltantes en cada columna
 faltantes_por_columna = df.isnull().sum()
 print("Cantidad de datos faltantes en cada columna:\n", faltantes_por_columna)
 
-#VERIFICACIÓN DE REGLAS DE RANGO Y DOMINIO
+#CALCULO DE LA CORRELACION
+print("La correlación es: ", df[["Año del hecho","Sexo de la victima"]].corr())
+
+
 
